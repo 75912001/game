29533dc64ceef9d0b2fd9fc5cf2e14fd6dd3e995
menglingchao <75912001@qq.com>
2025-11-25 15:44:29 +0800

    创建工程

--- airplane/client/src/game/draw.go
+++ airplane/client/src/game/draw.go
@@ -20,18 +20,23 @@ func (p *Game) Draw(screen *ebitenv2.Image) {
 	case StateMenu: // 菜单状态下绘制 UI
 		ui.GUIMgr.Draw(screen)
 	case StateBattling: // 游戏进行中,绘制飞机等游戏对象
-		// 绘制-用户-子弹
-		for _, bullet := range p.userBullets {
-			bullet.Draw(screen)
-		}
 		// 绘制-用户-飞机
 		if p.userPlane != nil {
 			p.userPlane.Draw(screen)
 		}
-	case StatePaused: // 暂停状态,绘制游戏对象和暂停提示
-		if p.userPlane != nil {
-			p.userPlane.Draw(screen)
+		// 绘制-用户-子弹
+		for _, bullet := range p.userBullets {
+			bullet.Draw(screen)
 		}
+		// 绘制-敌人-飞机
+		for _, plane := range p.enemyPlanes {
+			plane.DrawEnemy(screen)
+		}
+		// 绘制-敌人-子弹
+		for _, bullet := range p.enemyBullets {
+			bullet.Draw(screen)
+		}
+	case StatePaused: // 暂停状态,暂停提示
 		// 使用中文字体显示暂停信息
 		ui.Printf(screen, 280, 280, "*** 游戏暂停 ***")
 		ui.Printf(screen, 300, 310, "按 ESC 继续")
