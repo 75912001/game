6f6e66e17fe23e5a85b4308b0252f983705a7c03
menglingchao <75912001@qq.com>
2025-11-27 15:54:29 +0800

    创建工程

--- airplane/client/src/common/plane.go
+++ airplane/client/src/common/plane.go
@@ -4,3 +4,13 @@ type PlaneKey struct {
 	ID    uint32 // id
 	Level uint32 // 等级
 }
+
+type PlanePartType uint32
+
+const (
+	PlanePartTypeNose      PlanePartType = iota // 机头
+	PlanePartTypeBody                           // 机身
+	PlanePartTypeWingLeft                       // 机翼-左
+	PlanePartTypeWingRight                      // 机翼-右
+	PlanePartTypeMax                            // 最大值
+)
