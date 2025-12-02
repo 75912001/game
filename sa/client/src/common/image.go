508b6fca762b8f77f8143bf19ddcab67f0380aca
menglingchao <75912001@qq.com>
2025-12-02 17:37:20 +0800

    创建工程

--- sa/client/src/common/image.go
+++ sa/client/src/common/image.go
@@ -2,10 +2,11 @@ package common
 
 import (
 	ebitenv2 "github.com/hajimehoshi/ebiten/v2"
+	"saClient/src/proto"
 )
 
 type Image struct {
-	resType ResType
+	resType proto.ResType
 
 	resMajorID uint32 // 资源大类ID
 	resMinorID uint32 // 资源小类ID
--- sa/client/src/common/object.go
+++ sa/client/src/common/object.go
@@ -3,10 +3,10 @@ package common
 type Object struct {
 	image *Image
 
+	framesIdx int // 当前帧索引
+
 	x float64 // x 坐标
 	y float64 // y 坐标
-
-	scale float64 // 缩放比例(1.0为原始大小)
 
 	// 是否画出图像边界(调试用) 蓝色
 	debugDrawImageBounds bool
