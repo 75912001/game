1235bf563cefd96718a70d0979033ef6f962b985
menglingchao <75912001@qq.com>
2025-12-02 11:53:43 +0800

    创建工程

---
+++ sa/client/src/common/elemental.go
@@ -0,0 +1,10 @@
+package common
+
+type ElementalType uint32 // 元素类型
+
+const (
+	ElementalTypeEarth ElementalType = 1 // 土
+	ElementalTypeWater ElementalType = 2 // 水
+	ElementalTypeFire  ElementalType = 3 // 火
+	ElementalTypeWind  ElementalType = 4 // 风
+)
