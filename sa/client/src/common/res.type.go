955df4c3584c7086b3467eed70548346f6cafab7
menglingchao <75912001@qq.com>
2025-12-02 16:53:01 +0800

    创建工程

--- sa/client/src/common/res.type.go
+++
@@ -1,9 +0,0 @@
-package common
-
-type ResType string // 资源类型
-
-const (
-	ResTypeRole ResType = "role" // 角色
-	ResTypeMap  ResType = "map"  // 地图
-	ResTypeItem ResType = "item" // 物品
-)
---
+++ sa/client/src/common/role.go
@@ -0,0 +1,4 @@
+package common
+
+const RoleSpeed float64 = 2.0 // 角色默认移动速度
+const RoleScale float64 = 0.5 // 角色默认缩放比例
