c563cb7818d21bc039536e6ae1f8a61b6f02dd66
menglingchao <75912001@qq.com>
2025-12-02 11:43:02 +0800

    创建工程

---
+++ sa/client/src/common/image.go
@@ -0,0 +1,15 @@
+package common
+
+import ebitenv2 "github.com/hajimehoshi/ebiten/v2"
+
+type Image struct {
+	resType ResType
+
+	resMajorID uint32 // 资源大类ID
+	resMinorID uint32 // 资源小类ID
+
+	width  uint32 // 宽度
+	height uint32 // 高度
+
+	frames []*ebitenv2.Image // 动画帧
+}
