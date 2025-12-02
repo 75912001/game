9e618c6e82c039972df04e39a01d878135c3bde7
menglingchao <75912001@qq.com>
2025-12-02 13:42:10 +0800

    创建工程

--- airplane/client/src/ui/ui.go
+++ airplane/client/src/ui/ui.go
@@ -1,10 +1,10 @@
 package ui
 
 import (
-	"github.com/ebitenui/ebitenui"
+	ebitenui "github.com/ebitenui/ebitenui"
 	ebitenuiimage "github.com/ebitenui/ebitenui/image"
 	ebitenuiwidget "github.com/ebitenui/ebitenui/widget"
-	"github.com/hajimehoshi/ebiten/v2"
+	ebitenv2 "github.com/hajimehoshi/ebiten/v2"
 	imgcolor "image/color"
 )
 
@@ -19,7 +19,7 @@ type UIMgr struct {
 }
 
 func NewUIMgr() *UIMgr {
-	// 创建根容器，不使用布局管理器（允许手动定位）
+	// 创建根容器,不使用布局管理器(允许手动定位)
 	return &UIMgr{
 		ui: &ebitenui.UI{
 			Container: ebitenuiwidget.NewContainer(
@@ -45,6 +45,6 @@ func (p *UIMgr) Update() {
 }
 
 // Draw 绘制 UI
-func (p *UIMgr) Draw(screen *ebiten.Image) {
+func (p *UIMgr) Draw(screen *ebitenv2.Image) {
 	p.ui.Draw(screen)
 }
