5c4ce986f50ec1be3b7e1a07ea765041fd02699b
menglingchao <75912001@qq.com>
2025-12-02 17:48:21 +0800

    创建工程

---
+++ sa/client/src/main/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	ebitenv2 "github.com/hajimehoshi/ebiten/v2"
+	"image"
+	"log"
+	"os"
+	"path/filepath"
+	"saClient/src/common"
+	"saClient/src/game"
+)
+
+func main() {
+	// 设置窗口大小和标题
+	ebitenv2.SetWindowSize(common.WindowWidth, common.WindowHeight)
+	ebitenv2.SetWindowTitle(common.AppWindowTitle)
+
+	// 使用标准库加载图标
+	windowIconPath := filepath.Join(common.AppResDir, "window.icon.png")
+	iconFile, err := os.Open(windowIconPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	icon, _, err := image.Decode(iconFile)
+	_ = iconFile.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	ebitenv2.SetWindowIcon([]image.Image{icon})
+
+	// 创建并初始化游戏
+	_game := &game.Game{}
+	_game.Init()
+
+	// 运行游戏
+	err = ebitenv2.RunGame(_game)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
