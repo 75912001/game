bc164ad9b31807b04426f8d028f11a34cea4c421
menglingchao <75912001@qq.com>
2025-12-02 12:26:20 +0800

    创建工程

--- sa/client/src/common/image.type.go
+++
@@ -1,9 +0,0 @@
-package common
-
-type ImageType string
-
-const (
-	ImageTypeRole ImageType = "role"
-	ImageTypeMap  ImageType = "map"
-	ImageTypeItem ImageType = "item"
-)
--- sa/client/src/common/object.role.go
+++ sa/client/src/common/object.role.go
@@ -1,6 +1,7 @@
 package common
 
 const RoleSpeed float64 = 2.0 // 角色默认移动速度
+const RoleScale float64 = 0.5 // 角色默认缩放比例
 
 type ObjectRole struct {
 	id uint32 // ID
