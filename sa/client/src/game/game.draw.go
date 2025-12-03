01ec7b75856327871cef3a867b4c7f45033fb5f2
menglingchao <75912001@qq.com>
2025-12-03 10:36:02 +0800

    创建工程

---
+++ sa/client/src/game/game.draw.go
@@ -0,0 +1,31 @@
+package game
+
+import (
+	ebitenv2 "github.com/hajimehoshi/ebiten/v2"
+	"image/color"
+	"saClient/src/ui"
+)
+
+func (p *Game) Draw(screen *ebitenv2.Image) {
+	// 填充灰色背景
+	screen.Fill(color.RGBA{
+		R: 128,
+		G: 128,
+		B: 128,
+		A: 255,
+	})
+
+	// 根据游戏状态绘制不同的内容
+	switch p.state {
+	case StateMenu: // 菜单状态下绘制 UI
+		ui.GUIMgr.Draw(screen)
+	case StateScene: // 场景状态,绘制场景背景等
+	case StateBattling: // 战斗状态,绘制战斗相关内容
+	case StateGameOver: // 游戏结束,显示游戏结束信息
+		ui.Printf(screen, 280, 280, "*** 游戏结束 ***")
+	}
+	// 显示调试信息
+	if p.debugMsg != "" {
+		ui.Printf(screen, 0, 0, p.debugMsg)
+	}
+}
