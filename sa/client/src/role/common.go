836fac37e7893b04bdfe8361083af60a4399f119
menglingchao <75912001@qq.com>
2025-12-04 15:42:41 +0800

    创建工程

--- sa/client/src/role/common.go
+++ sa/client/src/role/common.go
@@ -1,4 +1,4 @@
 package role
 
-const RoleSpeed float64 = 2.0 // 角色默认移动速度
-const RoleScale float64 = 0.5 // 角色默认缩放比例
+const SpeedDefault float64 = 2.0 // 角色默认移动速度
+const ScaleDefault float64 = 0.5 // 角色默认缩放比例
