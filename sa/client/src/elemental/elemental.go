1ca24ac1582f0d61461bce71ade7172b29e10386
menglingchao <75912001@qq.com>
2025-12-04 15:17:08 +0800

    创建工程

--- sa/client/src/elemental/elemental.go
+++ sa/client/src/elemental/elemental.go
@@ -1,43 +1,46 @@
 package elemental
 
-import "saClient/src/proto"
+import (
+	"saClient/src/common"
+	"saClient/src/proto"
+)
 
 // Elemental 元素属性对象，总共10点可分配
 // 只能分配给相邻的两个属性或单个属性
 // 相邻关系:土-水,水-火,火-风,风-土(循环)
 
-func Get(assetRoleBaseMap map[uint32]uint64) (earth, water, fire, wind uint32) {
+func Get(assetRoleBaseMap common.AssetMap) (earth, water, fire, wind common.AssetQuantity) {
 	return GetEarth(assetRoleBaseMap), GetWater(assetRoleBaseMap), GetFire(assetRoleBaseMap), GetWind(assetRoleBaseMap)
 }
 
-func GetEarth(assetRoleBaseMap map[uint32]uint64) uint32 {
-	value, ok := assetRoleBaseMap[uint32(proto.AssetIDRoleBase_AssetIDRoleBase_ElementalEarth)]
+func GetEarth(assetRoleBaseMap common.AssetMap) common.AssetQuantity {
+	value, ok := assetRoleBaseMap[common.AssetID(proto.AssetIDRoleBase_AssetIDRoleBase_ElementalEarth)]
 	if ok {
-		return uint32(value)
+		return value
 	}
 	return 0
 }
 
-func GetWater(assetRoleBaseMap map[uint32]uint64) uint32 {
-	value, ok := assetRoleBaseMap[uint32(proto.AssetIDRoleBase_AssetIDRoleBase_ElementalWater)]
+func GetWater(assetRoleBaseMap common.AssetMap) common.AssetQuantity {
+	value, ok := assetRoleBaseMap[common.AssetID(proto.AssetIDRoleBase_AssetIDRoleBase_ElementalWater)]
 	if ok {
-		return uint32(value)
+		return value
 	}
 	return 0
 }
 
-func GetFire(assetRoleBaseMap map[uint32]uint64) uint32 {
-	value, ok := assetRoleBaseMap[uint32(proto.AssetIDRoleBase_AssetIDRoleBase_ElementalFire)]
+func GetFire(assetRoleBaseMap common.AssetMap) common.AssetQuantity {
+	value, ok := assetRoleBaseMap[common.AssetID(proto.AssetIDRoleBase_AssetIDRoleBase_ElementalFire)]
 	if ok {
-		return uint32(value)
+		return value
 	}
 	return 0
 }
 
-func GetWind(assetRoleBaseMap map[uint32]uint64) uint32 {
-	value, ok := assetRoleBaseMap[uint32(proto.AssetIDRoleBase_AssetIDRoleBase_ElementalWind)]
+func GetWind(assetRoleBaseMap common.AssetMap) common.AssetQuantity {
+	value, ok := assetRoleBaseMap[common.AssetID(proto.AssetIDRoleBase_AssetIDRoleBase_ElementalWind)]
 	if ok {
-		return uint32(value)
+		return value
 	}
 	return 0
 }
@@ -46,7 +49,7 @@ func GetWind(assetRoleBaseMap map[uint32]uint64) uint32 {
 // 规则：
 // 1. 总和必须为10点
 // 2. 只能分配给相邻的两个属性或单个属性
-func Validate(earth, water, fire, wind uint32) error {
+func Validate(earth, water, fire, wind common.AssetQuantity) error {
 	total := earth + water + fire + wind
 
 	// 验证总和是否为10
@@ -85,7 +88,7 @@ func Validate(earth, water, fire, wind uint32) error {
 }
 
 // isAdjacent 检查两个非零属性是否相邻
-func isAdjacent(earth, water, fire, wind uint32) bool {
+func isAdjacent(earth, water, fire, wind common.AssetQuantity) bool {
 	// 相邻关系：土(1)-水(2)、水(2)-火(3)、火(3)-风(4)、风(4)-土(1)
 	if earth > 0 && water > 0 {
 		return true // 土-水
