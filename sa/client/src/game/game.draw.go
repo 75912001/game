0bc82f1b1d3a8736c38300789357023ac7a3708c
menglingchao <75912001@qq.com>
2025-12-04 15:32:32 +0800

    创建工程

--- sa/client/src/game/game.draw.go
+++ sa/client/src/game/game.draw.go
@@ -17,15 +17,11 @@ func (p *Game) Draw(screen *ebitenv2.Image) {
 
 	// 根据游戏状态绘制不同的内容
 	switch p.state {
-	case StateMenu: // 菜单状态下绘制 UI
+	case State_StartMenu: // 开始菜单状态下绘制 UI
 		ui.GUIMgr.Draw(screen)
-	case StateScene: // 场景状态,绘制场景背景等
-	case StateBattling: // 战斗状态,绘制战斗相关内容
-	case StateGameOver: // 游戏结束,显示游戏结束信息
+	case State_Scene: // 场景状态,绘制场景背景等
+	case State_Battling: // 战斗状态,绘制战斗相关内容
+	case State_GameOver: // 游戏结束,显示游戏结束信息
 		ui.Printf(screen, 280, 280, "*** 游戏结束 ***")
-	}
-	// 显示调试信息
-	if p.debugMsg != "" {
-		ui.Printf(screen, 0, 0, p.debugMsg)
 	}
 }
--- sa/client/src/game/game.go
+++ sa/client/src/game/game.go
@@ -1,35 +1,25 @@
 package game
 
 import (
+	"saClient/src/role"
 	"saClient/src/ui"
 )
 
-// GameState 游戏状态
-type GameState int
-
-const (
-	StateMenu     GameState = iota // 菜单状态
-	StateScene                     // 场景中
-	StateBattling                  // 战斗中
-	StateGameOver                  // 游戏结束
-)
-
 type Game struct {
-	debugMsg string
-	state    GameState // 当前游戏状态
-
-	user *Role
+	state State // 当前游戏状态
+	user  *role.Role
 }
 
 func (p *Game) Init() {
 	// 初始化为菜单状态
-	p.state = StateMenu
+	p.state = State_StartMenu
 
 	// 添加一个开始按钮
 	ui.GUIButtonMgr.AddButton("startBtn", 300, 250, 200, 50, "开始",
 		func() (ui.ButtonAction, error) {
 			// 点击开始按钮后,切换到游戏状态
-			p.state = StateScene
+			p.state = State_Scene
+			// 销毁按钮
 			return ui.ButtonActionDestroy, nil
 		},
 	)
