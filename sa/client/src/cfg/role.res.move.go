6cbc4e2bd58f839d5c8267c97ee1fc05070d6196
menglingchao <75912001@qq.com>
2025-12-04 18:53:28 +0800

    创建工程

---
+++ sa/client/src/cfg/role.res.move.go
@@ -0,0 +1,11 @@
+package cfg
+
+import (
+	ebitenv2 "github.com/hajimehoshi/ebiten/v2"
+	"saClient/src/proto"
+)
+
+type RoleMove struct {
+	Frames    [proto.RoleDirection_RoleDirection_Max]*ebitenv2.Image  // 动画帧
+	FrameInfo [proto.RoleDirection_RoleDirection_Max][]*CfgRoleSprite // 向上-动画帧信息 配置表中
+}
