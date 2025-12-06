804c9393538d84054369425d63df817843e40cd2
menglingchao <75912001@qq.com>
2025-12-06 09:54:02 +0800

    创建工程

--- sa/client/src/cfg/res/common.go
+++ sa/client/src/cfg/res/common.go
@@ -13,23 +13,6 @@ type Rect struct {
 	Height int `json:"h"` // 高度
 }
 
-// GetNameByAssetType 根据资产类型获取资源名称
-// 例如: 枚举 AssetType_AssetType_Role 的字符串为 "AssetType_Role" 返回 "role"
-func GetNameByAssetType(assetType proto.AssetType) string {
-	value, ok := proto.AssetType_name[int32(assetType)]
-	if ok {
-		if i := strings.Index(value, "_"); i >= 0 && i+1 < len(value) {
-			return strings.ToLower(value[i+1:])
-		}
-		name := strings.ToLower(value)
-		if name == "max" { // 排除枚举中的最大值,该值不表示具体资源类型
-			return "unknow"
-		}
-		return name
-	}
-	return "unknow"
-}
-
 // GetNameByRoleDirection 根据角色方向获取名称
 // 例如: 枚举 RoleDirection_RoleDirection_Up 的字符串为 "RoleDirection_Up" 返回 "up"
 func GetNameByRoleDirection(roleDirection proto.RoleDirection) string {
