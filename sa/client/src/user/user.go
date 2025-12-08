a6bfbb044fa490f723c48fb627e6d3335531d684
menglingchao <75912001@qq.com>
2025-12-08 17:37:06 +0800

    创建工程

--- sa/client/src/user/user.go
+++ sa/client/src/user/user.go
@@ -2,13 +2,21 @@ package user
 
 import (
 	"saClient/src/common"
+	"saClient/src/proto"
 )
 
 type User struct {
+	record *proto.UserRecord
 }
 
 func NewUser() *User {
 	return &User{}
+}
+
+func (p *User) Login(account string, password string) error {
+	p.record = &proto.UserRecord{}
+	// 初始化用户数据 todo menglc 模拟数据
+	return nil
 }
 
 func (p *User) SelectRole(roleUUID common.RoleUUID) error {
