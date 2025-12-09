e8ba39361c959a6a592292601ffc833ffad62852
menglingchao <75912001@qq.com>
2025-12-09 19:17:05 +0800

    创建工程

--- sa/client/src/user/role/role.draw.go
+++ sa/client/src/user/role/role.draw.go
@@ -2,19 +2,15 @@ package role
 
 import (
 	ebitenv2 "github.com/hajimehoshi/ebiten/v2"
-	"saClient/src/proto"
+	"saClient/src/common"
 )
 
 func (p *Role) Draw(screen *ebitenv2.Image) {
-	p.scene.Draw(screen)
+	// 绘制场景
+	p.scene.Draw(screen, p.camera.X, p.camera.Y)
 
-	direction := p.GetValueU32(proto.AssetIDRecord_AssetIDRecord_Direction)
-	frames := p.cfgRole.ResRole.Move.Frames[direction]
-	frameImage := frames[p.frameIdx%uint32(len(frames))]
-	// 绘制
+	// 绘制角色
 	op := &ebitenv2.DrawImageOptions{}
-	x := p.GetValueU32(proto.AssetIDRecord_AssetIDRecord_X)
-	y := p.GetValueU32(proto.AssetIDRecord_AssetIDRecord_Y)
-	op.GeoM.Translate(float64(x), float64(y))
-	screen.DrawImage(frameImage, op)
+	op.GeoM.Translate(float64(common.ScreenWidth/2-p.roleSprite.roleImageSprite.Frame.Width/2), float64(common.ScreenHeight/2-p.roleSprite.roleImageSprite.Frame.Height/2))
+	screen.DrawImage(p.roleSprite.image, op)
 }
--- sa/client/src/user/scene/scene.map.go
+++ sa/client/src/user/scene/scene.map.go
@@ -1,13 +1,37 @@
 package scene
 
+import (
+	ebitenv2 "github.com/hajimehoshi/ebiten/v2"
+	"saClient/src/cfg"
+	"saClient/src/common"
+)
+
 // Map 场景-地图
 type Map struct {
+	id  common.AssetID // 地图ID
+	cfg *cfg.Map       // 地图配置
+}
+
+func NewMap(mapID common.AssetID) *Map {
+	m := &Map{
+		id: mapID,
+	}
+	// 从配置管理器获取地图配置
+	m.cfg = cfg.GMapMgr.Maps.Get(mapID)
+	return m
 }
 
 func (p *Map) Update() {
 
 }
 
-func (p *Map) Draw() {
-
+func (p *Map) Draw(screen *ebitenv2.Image, cameraX, cameraY int) {
+	op := &ebitenv2.DrawImageOptions{}
+	// 相机能拍到的区域是以屏幕中心为基准的
+	// 计算左上角的位置
+	screenX := cameraX - common.ScreenWidth/2
+	screenY := cameraY - common.ScreenHeight/2
+	// 将地图图片绘制到屏幕上
+	op.GeoM.Translate(float64(-screenX), float64(-screenY))
+	screen.DrawImage(p.cfg.ResMap.Image, op)
 }
