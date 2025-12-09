6f08cb3047ec7967bd262404fd1a239d69e57489
menglingchao <75912001@qq.com>
2025-12-09 09:55:00 +0800

    创建工程

--- sa/client/src/user/user.update.go
+++ sa/client/src/user/user.update.go
@@ -1,5 +1,15 @@
 package user
 
-func (p *User) Update() error {
-	return nil
+import (
+	"saClient/src/common"
+	"saClient/src/user/role"
+)
+
+func (p *User) Update() {
+	p.roleMgr.Roles.Foreach(
+		func(key common.RoleUUID, role *role.Role) (isContinue bool) {
+			role.Update()
+			return true
+		},
+	)
 }
