c281af0994965b6194f627d6a3801eb1fba7c047
menglingchao <75912001@qq.com>
2025-12-09 09:58:16 +0800

    创建工程

--- sa/client/src/user/user.draw.go
+++ sa/client/src/user/user.draw.go
@@ -2,8 +2,18 @@ package user
 
 import (
 	ebitenv2 "github.com/hajimehoshi/ebiten/v2"
+	"saClient/src/ui"
 )
 
 func (p *User) Draw(screen *ebitenv2.Image) {
+	// 填充灰色背景
+	//screen.Fill(color.RGBA{
+	//	R: 128,
+	//	G: 128,
+	//	B: 128,
+	//	A: 255,
+	//})
 
+	p.role.Draw(screen)
+	ui.GUIMgr.Draw(screen)
 }
