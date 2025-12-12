65bb3ecb8f5dbe5b46124b97de1b0232a85c9719
lingchao <75912001@qq.com>
2025-12-13 00:53:58 +0800

    Changes

--- sa/client/src/game/game.draw.go
+++ sa/client/src/game/game.draw.go
@@ -1,9 +1,18 @@
 package game
 
 import (
+	"saClient/src/cfg"
+	"saClient/src/ui"
+
 	ebitenv2 "github.com/hajimehoshi/ebiten/v2"
 )
 
 func (p *Game) Draw(screen *ebitenv2.Image) {
 	p.user.Draw(screen)
+
+	// 在屏幕右上角显示帧率
+	fps := ebitenv2.ActualFPS()
+	x := float32(cfg.GCommon.ScreenMaxWidth - 100)
+	y := float32(10)
+	ui.Printf(screen, x, y, "FPS: %.1f", fps)
 }
