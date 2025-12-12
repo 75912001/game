4500675e1743444879ce2a358b2d8e98965042ac
menglingchao <75912001@qq.com>
2025-12-12 11:33:58 +0800

    切换地图

--- sa/client/res/tiled/tsx/map.isometric_grass_and_water.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="isometric_grass_and_water" tilewidth="64" tileheight="32" tilecount="48" columns="4">
- <image source="isometric_grass_and_water.png" width="256" height="384"/>
-</tileset>
