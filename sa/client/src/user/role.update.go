12de68eb1cec40a0c0a5b82e7560e0568c583f61
lingchao <75912001@qq.com>
2025-12-14 21:15:26 +0800

    Changes

--- sa/client/src/user/role.update.go
+++ sa/client/src/user/role.update.go
@@ -4,7 +4,7 @@ import "saClient/src/user/camera"
 
 func (p *Role) Update() {
 	// 更新当前场景的过渡动画
-	isActive, isCloseComplete, _ := p.scene.UpdateTransition()
+	isActive, isCloseComplete, _ := p.scene.transition.Update()
 
 	// 转场退出完成，切换到新场景
 	if isCloseComplete && p.pendingScene != nil {
@@ -13,7 +13,7 @@ func (p *Role) Update() {
 		p.sprite.bottomCenterWorldX, p.sprite.bottomCenterWorldY = p.scene._map.cfg.IsometricCT.T2W(p.pendingTX, p.pendingTY)
 		p.pendingScene = nil
 		// 新场景转场进入
-		p.scene.TransitionIn()
+		p.scene.transition.TransitionIn()
 		p.UpdateWithAction()
 		return
 	}
