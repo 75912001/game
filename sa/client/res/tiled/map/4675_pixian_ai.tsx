e675119729471ababd6ce8360cacfa4243ec03a3
lingchao <75912001@qq.com>
2025-12-18 00:31:38 +0800

    Changes

---
+++ sa/client/res/tiled/map/4675_pixian_ai.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="4675_pixian_ai" tilewidth="424" tileheight="424" tilecount="1" columns="1">
+ <image source="4675_pixian_ai.png" width="424" height="424"/>
+</tileset>
