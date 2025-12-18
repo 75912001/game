547559d6aba23da96dc1e36dd18fb1f339e0113e
menglingchao <75912001@qq.com>
2025-12-18 15:45:49 +0800

    切换地图

---
+++ sa/client/res/tiled/map/7000002.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="7000002" tilewidth="355" tileheight="328" tilecount="1" columns="1">
+ <properties>
+  <property name="verticalSlicePixel" type="int" value="64"/>
+ </properties>
+ <image source="../../building/7000002.png" width="355" height="328"/>
+</tileset>
